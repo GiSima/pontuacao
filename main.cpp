6261d4c1d07899500782b2e7bd97abf3add62b96
Giovane Simamoto <60979790+GiSima@users.noreply.github.com>
2020-05-19 07:55:01 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -4,14 +4,22 @@ using namespace std;
 
 int main()
 {
-    int compras;
+    int compras, atraso;
     double ticket;
+    char forma;
 
+    //cabeçalho
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "--------------------------------" << endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
 
+    //leitura volume
     cout << endl << "Quantas compras o cliente fez no ultimo ano? "; cin >> compras;
     cout << "Qual o ticket medio? "; cin >> ticket;
+
+    //leitura outros dados
+    cout << endl << "Quantas vezes o cliente atrasou o pagamento? "; cin >> atraso;
+    cout << "A maioria das compras foi em dinheiro, cartao, ou boleto (D/C/B)? "; cin >> forma;
+
     return 0;
 }
