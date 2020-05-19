b2a539e8ec769d1ef7fdbb3f28c473c3e1cde0b5
Giovane Simamoto <60979790+GiSima@users.noreply.github.com>
2020-05-19 07:51:26 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@ using namespace std;
 
 int main()
 {
+    int compras;
+    double ticket;
+
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "--------------------------------" << endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
 
+    cout << endl << "Quantas compras o cliente fez no ultimo ano? "; cin >> compras;
+    cout << "Qual o ticket medio? "; cin >> ticket;
     return 0;
 }
